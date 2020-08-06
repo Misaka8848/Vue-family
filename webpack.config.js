901fc50795f3ff89c39e8d56cf622934619a78ed
Z <467342345@qq.com>
2020-08-07 00:06:35 +0800

    使用视频中的开发环境，完成2-1章节项目目录结构升级，以及webpack配置的优化。

--- webpack.config.js
+++ webpack.config.js
@@ -1,112 +1,107 @@
-const path = require('path')
-const HTMLPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-const ExtractPlugin = require('extract-text-webpack-plugin')
+const path = require("path");
+const HTMLPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const ExtractPlugin = require("extract-text-webpack-plugin");
 
-const isDev = process.env.NODE_ENV === 'development'
+const isDev = process.env.NODE_ENV === "development";
 
 const config = {
-  target: 'web',
-  entry: path.join(__dirname, 'src/index.js'),
+  target: "web",
+  entry: path.join(__dirname, "../src/index.js"),
   output: {
-    filename: 'bundle.[hash:8].js',
-    path: path.join(__dirname, 'dist')
+    filename: "bundle.[hash:8].js",
+    path: path.join(__dirname, "dist"),
+
   },
   module: {
     rules: [
       {
         test: /\.vue$/,
-        loader: 'vue-loader'
+        loader: "vue-loader",
       },
       {
         test: /\.jsx$/,
-        loader: 'babel-loader'
+        loader: "babel-loader",
       },
       {
         test: /\.(gif|jpg|jpeg|png|svg)$/,
         use: [
           {
-            loader: 'url-loader',
+            loader: "url-loader",
             options: {
               limit: 1024,
-              name: '[name]-aaa.[ext]'
-            }
-          }
-        ]
-      }
-    ]
+              name: " [name]-[hash].[ext]",
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: isDev ? '"development"' : '"production"'
-      }
-    }),
-    new HTMLPlugin()
-  ]
-}
+    new webpack.DefinePlugin(),
+    new HTMLPlugin(),
+  ],
+};
 
 if (isDev) {
   config.module.rules.push({
     test: /\.styl/,
     use: [
-      'style-loader',
-      'css-loader',
+      "style-loader",
+      "css-loader",
       {
-        loader: 'postcss-loader',
+        loader: "postcss-loader",
         options: {
           sourceMap: true,
-        }
+        },
       },
-      'stylus-loader'
-    ]
-  })
-  config.devtool = '#cheap-module-eval-source-map'
+      "stylus-loader",
+    ],
+  });
+  config.devtool = "#cheap-module-eval-source-map";
   config.devServer = {
     port: 8000,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     overlay: {
       errors: true,
     },
-    hot: true
-  }
+    hot: true,
+  };
   config.plugins.push(
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin()
-  )
+  );
 } else {
   config.entry = {
-    app: path.join(__dirname, 'src/index.js'),
-    vendor: ['vue']
-  }
-  config.output.filename = '[name].[chunkhash:8].js'
-  config.module.rules.push(
-    {
-      test: /\.styl/,
-      use: ExtractPlugin.extract({
-        fallback: 'style-loader',
-        use: [
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: true,
-            }
+    app: path.join(__dirname, "src/index.js"),
+    vendor: ["vue"],
+  };
+  config.output.filename = "[name].[chunkhash:8].js";
+  config.module.rules.push({
+    test: /\.styl/,
+    use: ExtractPlugin.extract({
+      fallback: "style-loader",
+      use: [
+        "css-loader",
+        {
+          loader: "postcss-loader",
+          options: {
+            sourceMap: true,
           },
-          'stylus-loader'
-        ]
-      })
-    },
-  )
+        },
+        "stylus-loader",
+      ],
+    }),
+  });
   config.plugins.push(
-    new ExtractPlugin('styles.[contentHash:8].css'),
+    new ExtractPlugin("styles.[contentHash:8].css"),
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
+      name: "vendor",
     }),
     new webpack.optimize.CommonsChunkPlugin({
-      name: 'runtime'
+      name: "runtime",
     })
-  )
+  );
 }
 
-module.exports = config
+module.exports = config;
